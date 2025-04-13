1a28989918bc916a5b6c8b17e7cefe388faa349d
kjljixx <kjljixx@gmail.com>
2025-04-13 09:42:09 -0400

    Add robot telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Robot.java
@@ -1,6 +1,11 @@
 package org.firstinspires.ftc.teamcode.utils;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 //Allow configuration variables to be tuned without pushing code
 //with FTC Dashboard (https://acmerobotics.github.io/ftc-dashboard/features#configuration-variables)
@@ -9,9 +14,15 @@ public class Robot{
   //Constants
 
   //Mechanisms, IMU, etc.
+  public static MultipleTelemetry telemetry;
   public static boolean initialized = false;
 
-  public static void initialize(){
+  public static void initialize(HardwareMap hardwareMap, Telemetry dsTelemetry){
+    telemetry = new MultipleTelemetry(
+        dsTelemetry, // Driver Station telemetry
+        FtcDashboard.getInstance().getTelemetry() // Dashboard telemetry
+    );
+
     initialized = true;
   }
 }
