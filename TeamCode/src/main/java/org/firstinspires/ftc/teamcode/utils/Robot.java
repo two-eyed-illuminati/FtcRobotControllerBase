346b7054e5ee7e2067c4863420d424627e3c883b
kjljixx <kjljixx@gmail.com>
2025-04-13 20:41:32 -0400

    Add Drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Robot.java
@@ -3,9 +3,11 @@ package org.firstinspires.ftc.teamcode.utils;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.utils.rr.MecanumDrive;
 
 //Allow configuration variables to be tuned without pushing code
 //with FTC Dashboard (https://acmerobotics.github.io/ftc-dashboard/features#configuration-variables)
@@ -14,13 +16,15 @@ public class Robot{
   //Constants
 
   //Mechanisms, IMU, etc.
+  public static MecanumDrive drive;
   public static MultipleTelemetry telemetry;
   public static boolean initialized = false;
 
   public static void initialize(HardwareMap hardwareMap, Telemetry dsTelemetry){
+    drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
     telemetry = new MultipleTelemetry(
-        dsTelemetry, // Driver Station telemetry
-        FtcDashboard.getInstance().getTelemetry() // Dashboard telemetry
+            dsTelemetry, // Driver Station telemetry
+            FtcDashboard.getInstance().getTelemetry() // Dashboard telemetry
     );
 
     initialized = true;
